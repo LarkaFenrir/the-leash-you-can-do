14ce14738acd3d186b1ce8a27c19ff1d045481af
martina-colombo-42 <martina-colombo@hotmail.com>
2026-04-07 11:59:42 +0200

    feat: basic user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+
+    # TODO: find a way to verify people can't simply put 'staff member'
+
+    ROLE = (
+        ('V', 'Volunteer')
+        ('SM', 'Staff Member')
+    )
+
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    username = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
+    role = models.CharField(choices=ROLE)
+    is_owner = models.BooleanField()
+
+    def __str__(self):
+        return self.username
