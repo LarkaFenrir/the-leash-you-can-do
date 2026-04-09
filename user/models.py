15b292d27c6193dafe725e85e64280e315e69179
martina-colombo-42 <martina-colombo@hotmail.com>
2026-04-09 14:54:19 +0200

    Improve custom user model for shelter members
    Changes:
    - re-write 'first_name' and 'last_name' to make them obligatory fields
    - inherit the password from the AbstractUser to hash it, improving security
    - forbid any user from choosing their roles, so that only admin can
    - set a max length for the role choices, which are now set as 'volunteer' by default
    - change the name of the variable to indicate if the user has pets, set a default, and an explanatory text
    - use 'email' to login, not 'username'
    - add verbose names
    - set a more detail debugging string

--- user/models.py
+++ user/models.py
@@ -4,21 +4,41 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    """Custom user model for shelter members.
 
-    # TODO: find a way to verify people can't simply put 'staff member'
-
-    ROLE = (
-        ('V', 'Volunteer')
+    Features:
+    - every field is required
+    - email is a unique field and it is required to login
+    """
+    ROLE_CHOICES = (
+        ('V', 'Volunteer'),
         ('SM', 'Staff Member')
     )
 
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
-    username = models.CharField(max_length=20, unique=True)
+    # add first_name and last_name to make them required
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
+
+    # inherit the password to hash it, improving security
+
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=20)
-    role = models.CharField(choices=ROLE)
-    is_owner = models.BooleanField()
+
+    # paired with omitting the choice in the form
+    # only admin can change a user's role from their admin panel
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES, default='V')
+
+    has_pets = models.BooleanField(
+        default=False,
+        help_text='Do you own any pets?'
+    )
+
+    # users will use email instead of username as login info
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    class Meta:
+        verbose_name = 'Shelter Member'
+        verbose_name_plural = 'Shelter Members'
 
     def __str__(self):
-        return self.username
+        return f'{self.get_full_name()} ({self.email})'
